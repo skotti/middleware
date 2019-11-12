9921d482212f49ffb58b43bfdffcd6e032ccdee8
aruzhans <anastasiia.ruzhanskaia@inf.ethz.ch>
2019-11-12 20:29:27 +0100

    Changed main script

--- superex.sh
+++ superex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source environment.sh
-populate_servers=($server1)
+populate_servers=($server1ip)
 populate_port=11210
 populate_time=60
 
@@ -17,26 +17,26 @@ for v in "${value_size[@]}"; do
     done
     echo "SERVERS POPULATED"
     for t in "${n_workers[@]}"; do
-	ssh azureuser@$middleware1 /home/azureuser/asl-project-2019-ruzhanskaia/start.sh $middleware1 11211 $t
+	ssh azureuser@$middleware1ip '/home/azureuser/asl-project-2019-ruzhanskaia/start.sh $middleware1ip 11211 $t'
 	sleep 10s
-	echo "MIDDLEWARE LAUNCHED"
+	echo "MIDDLEWARE LAUNCHEDip"
         for c in "${n_clients[@]}"; do
             for value in {1..3}
 	    do
 		# run script on client1
-		/home/azureuser/asl-project-2019-ruzhanskaia/experiment.sh $middleware1 11211 $test_time $memtier_threads $v $c &>> client1.log &
+		/home/azureuser/asl-project-2019-ruzhanskaia/experiment.sh $middleware1iipp 11211 $test_time $memtier_threads $v $c &>> client1.log &
 		echo "CLIENT1 RUN"
 		# store pid
 		pid1=$!
 		echo "$pid1"
 		# run script on client2
-		ssh azureuser@$client2 /home/azureuser/asl-project-2019-ruzhanskaia/experiment.sh $middleware1 11211 $test_time $memtier_threads $v $c &>> client2.log &
+		ssh azureuser@$client2ip '/home/azureuser/asl-project-2019-ruzhanskaia/experiment.sh $middleware1ip 11211 $test_time $memtier_threads $v $c &>> client2.log &'
 		echo "CLIENT2 RUN"
 		# store pid
 		pid2=$!
 		echo "$pid2"
 		# run script on client3
-		ssh azureuser@$client3 /home/azureuser/asl-project-2019-ruzhanskaia/experiment.sh $middleware1 11211 $test_time $memtier_threads $v $c &>> client3.log &
+		ssh azureuser@$client3ip '/home/azureuser/asl-project-2019-ruzhanskaia/experiment.sh $middleware1ip 11211 $test_time $memtier_threads $v $c &>> client3.log &'
 		echo "CLIENT3 RUN"
 		#store pid
 		pid3=$!
@@ -50,7 +50,7 @@ for v in "${value_size[@]}"; do
 		sleep 10s
 	    done
 	done
-        ssh azureuser@$middleware1 /home/azureuser/asl-project-2019-ruzhanskaia/stop.sh
+        ssh azureuser@$middleware1ip '/home/azureuser/asl-project-2019-ruzhanskaia/stop.sh'
         sleep 10s
     done
 done
