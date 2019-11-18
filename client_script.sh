c729dd6da1766df11714691f430f901e97ecb278
aruzhans <anastasiia.ruzhanskaia@inf.ethz.ch>
2019-11-18 20:05:41 +0100

    Added locale script

--- client_script.sh
+++ client_script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt install libevent-dev git unzip ant openjdk-8-jdk build-essential autoconf automake libpcre3-dev libevent-dev pkg-config zlib1g-dev
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 git clone https://github.com/RedisLabs/memtier_benchmark.git
---
+++ locale_update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
