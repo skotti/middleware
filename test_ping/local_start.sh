ccd156edbf38e3bb44f8b39618767c5ea2304f85
aruzhans <anastasiia.ruzhanskaia@inf.ethz.ch>
2019-11-27 07:28:59 +0100

    corrected addree in test ping

--- test_ping/local_start.sh
+++ test_ping/local_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar dist/middleware-18-904-029.jar -l localhost -p 11211 -t 1 -s false -m localhost:11210
+java -jar dist/middleware-18-904-029.jar -l 10.0.0.5 -p 11211 -t 1 -s false -m localhost:11210
